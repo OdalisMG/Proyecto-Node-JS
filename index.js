e0b39839a0c97337d4221a5cc06c57cb3ad78dcc
OdalisMG <Godalis64gmail.com>
2023-06-28 21:07:09 -0500

    28/6/2023

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Hola mundo");
+
+let enojo = 50;
+let tiempo = 100;
+
+console.log(enojo);
+
+console.log(enojo + tiempo);
+
+enojo = "Efectivamente esta molesto Odalis";
+
+console.log(enojo);
